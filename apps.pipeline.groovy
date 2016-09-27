6231dea862e7512712c7b28d2460195bab32db22
George McIntosh <george@elevenware.com>
2016-09-27 13:01:16 +0100

    actual pipeline

--- apps.pipeline.groovy
+++ apps.pipeline.groovy
@@ -3,13 +3,14 @@ def envs = ['integration', 'qa', 'staging', 'prod']
 for(int i = 0; i < envs.size(); i++) {
 
   String env = envs[i]
-  stage name: env, concurrency: 1
+  stage name: env, concurrency: 1 {
     node {
       echo "Running $env"
       if(i > 1) {
         String last = envs[i=1]
         input message: "Does http://localhost:8888/$last/ look good?"
       }
-      
     }
+      
+  }
 }
