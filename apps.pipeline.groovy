1b75fc1136eba52eec9c4ed21388beeaa9d54e10
George McIntosh <george@elevenware.com>
2016-09-27 13:04:30 +0100

    actual pipeline

--- apps.pipeline.groovy
+++ apps.pipeline.groovy
@@ -6,7 +6,7 @@ for(int i = 0; i < envs.size(); i++) {
   stage(name: env, concurrency: 1) 
     node {
       echo "Running $env"
-      if(i > 1) {
+      if(i > 0) {
         String last = envs.get(i - 1)
         input message: "Does http://localhost:8888/$last/ look good?"
       }
