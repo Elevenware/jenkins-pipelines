1779bd2d0fe2f0008c780ec7033f5f90dbc5bd0b
George McIntosh <george@elevenware.com>
2016-09-27 14:27:59 +0100

    spiking

--- tracer.groovy
+++ tracer.groovy
@@ -1,3 +1,7 @@
-call() {
-    println "OHAI"
+def call(body) {
+    def config = [:]
+    body.resolveStrategy = Closure.DELEGATE_FIRST
+    body.delegate = config
+    body()
+    echo "RUNNING"
 }
