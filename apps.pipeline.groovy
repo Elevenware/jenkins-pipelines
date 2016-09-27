b3ee7cd0649c11dcfac578e9d2edcf0bdf4d0783
George McIntosh <george@elevenware.com>
2016-09-27 12:56:03 +0100

    more groovy

--- apps.pipeline.groovy
+++ apps.pipeline.groovy
@@ -1,18 +1,8 @@
-stage name: 'integration', concurrency: 1
-  node {
-     echo 'This is inegration'
-     input message: "Does http://localhost:8888/integration/ look good?"
-  }
-stage name: 'QA', concurrency: 1
-  node {
-     echo 'This is QA'
-  }
-stage name: 'staging', concurrency: 1
-  node {
-     echo 'This is staging'
-     input message: "Does http://localhost:8888/qa/ look good?"
-  }
-stage name: 'production', concurrency: 1
-  node {
-     echo 'This is production'
-  }
+['integration', 'qa', 'staging', 'prod'].each { env ->
+ 
+  stage name: env, concurrency: 1
+    node {
+      echo "Running $env"
+      input message: "Does http://localhost:8888/$env/ look good?"
+    }
+}
